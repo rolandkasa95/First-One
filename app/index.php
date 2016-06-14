c488435127f37ed0a7b995b8d6eef2a06edfa483
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-14 15:50:02 +0300

    Update: modified index.php

--- app/index.php
+++ app/index.php
@@ -1,19 +1,22 @@
 <html>
 
   <ul>
-    <li>
-      <a href="/php_oop/learning.php">Monday</a>
-    </li>
-    <li>
-      <ul> Tuesday
+      <li>
+        <a href="/CRUD/ajax_example.html">CRUD with mysql (working)</a>
+      </li>
         <li>
-          <a href="/php_oop/prob1.php">Learning Objects and Classes</a>
+            <a href="/php_oop/learning.php">Monday</a>
         </li>
         <li>
-          <a href="/php_oop/harvesting.php">Harvesting season for Fruits</a>
+            <ul> Tuesday
+                <li>
+                    <a href="/php_oop/prob1.php">Learning Objects and Classes</a>
+                </li>
+                <li>
+                    <a href="/php_oop/harvesting.php">Harvesting season for Fruits</a>
+                </li>
+            </ul>
         </li>
-      </ul>
-    </li>
   </ul>
 
 </html>
